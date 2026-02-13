79c9ba90f4008b9b8b46306b75ba9fbcad1b4bda
SetyVII <dreaming4splendid@protonmail.com>
2026-02-13 13:16:48 +0100

    Switch to PostgreSQl dialect and lower force

--- src/main/java/com/example/grupo5/entity/CartItem.java
+++ src/main/java/com/example/grupo5/entity/CartItem.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "cart_items", uniqueConstraints = @UniqueConstraint(columnNames = {"customer_id", "product_id"}))
 public class CartItem {
-    // Hola Ivan
+    // Hola Ivan dos
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     @Column(name = "cart_item_id")
